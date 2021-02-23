807d0dd838bbcd7ce92a7e95b67b1a2ef4e74dde
chrishj59 <chrishj@btinternet.com>
2021-02-23 07:57:37 +0000

    Start up message 2

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-	console.log('starting up..... ');
+	console.log('start up..... ');
 
 	try {
 		if (!process.env.JWT_KEY) {
